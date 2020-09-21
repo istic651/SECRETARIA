821ba12cf7fd2ab0290ff03d95ed349b9cead657
istic651 <istic651@gmail.com>
2020-09-21 20:22:09 -0300

    actualice

--- pages/forms/inscripcionProfesores.php
+++ pages/forms/inscripcionProfesores.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Sistema de Gestión | Istic</title>
+        <title>Sistema de Gestión | Isti</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- bootstrap 3.0.2 -->
         <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
